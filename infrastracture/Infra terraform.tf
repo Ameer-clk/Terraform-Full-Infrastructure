e5aac471ef6374d9b6d555c0bdb353209ad69107
Ameer ALi <114929844+Ameer-clk@users.noreply.github.com>
2022-11-05 18:59:06 +0530

    Add files via upload

---
+++ infrastracture/Infra terraform.tf
@@ -0,0 +1,103 @@
+  provider "aws" {
+  region = "us-east-1"
+  access_key = "AKIA3RFYQ64PLP62UE67"
+  secret_key = "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"
+}
+  resource "aws_vpc" "main" {
+  cidr_block = "10.1.0.0/16"
+}
+
+resource "aws_subnet" "mysubnet1" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.1.10.0/24"
+}
+
+resource "aws_subnet" "mysubnet2" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.1.20.0/24"
+}
+
+resource "aws_internet_gateway" "inetrnetgateway" {
+  vpc_id = aws_vpc.main.id
+}
+
+resource "aws_route_table" "myroutetable" {
+  vpc_id = aws_vpc.main.id
+}
+
+resource "aws_route_table_association" "myroutetable" {
+  subnet_id      = aws_subnet.mysubnet1.id
+  route_table_id = aws_route_table.myroutetable.id
+}
+
+resource "aws_security_group" "serversg" {
+  name        = "serversg"
+  description = "For new infra"
+  vpc_id      = aws_vpc.main.id
+  image_id    = "ami-06640050dc3f556bb"
+}
+
+resource "aws_ami" "example" {
+  name                = "terraform-example"
+  virtualization_type = "hvm"
+  root_device_name    = "/dev/xvda"
+  imds_support        = "v2.0"
+  ebs_block_device {
+    device_name = "/dev/xvda"
+    snapshot_id = "snap-0dad6dd3afcafa13c"
+     volume_size = 100
+  }
+}
+
+resource "aws_launch_template" "mytemplate" {
+  name = "mytemplate"
+  image_id = "ami-06640050dc3f556bb"
+   instance_type = "t2.micro"
+   key_name = "demo"
+}
+
+resource "aws_lb_target_group" "newtargetgp" {
+  name     = "newtargetgp"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.main.id
+}
+resource "aws_lb" "test" {
+  name               = "test-lb-tf"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.serversg.id]
+  subnets            = [aws_subnet.mysubnet1.id,aws_subnet.mysubnet2.id]
+}
+
+resource "aws_autoscaling_group" "newASG" {
+  availability_zones = ["us-east-1a"]
+  desired_capacity   = 1
+  max_size           = 2
+  min_size           = 1
+
+  launch_template 
+    id      = aws_launch_template.mytemplate.id
+    version = "$Latest"
+  }
+}
+
+resource "aws_sns_topic" "newtopic" {
+  name = "newtopic"
+  kms_master_key_id = "alias/aws/sns"
+}
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
