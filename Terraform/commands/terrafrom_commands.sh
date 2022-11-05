59545de7ccd810fd3a347578387f0e807468aaf9
Ameer ALi <114929844+Ameer-clk@users.noreply.github.com>
2022-11-05 18:56:28 +0530

    Add files via upload

---
+++ Terraform/commands/terrafrom_commands.sh
@@ -0,0 +1,21 @@
+# Commands that to exicute the Terrafrom
+
+
+# terrafrom init
+_________________
+
+To exicute check cloud provide and our code 
+
+
+# terraform validate
+
+To check our configuration is correct 
+
+# terraform plan 
+
+To check what are the service we are adding in the infrastructure
+
+# terraform apply 
+
+To exicute the infrastructure
+
