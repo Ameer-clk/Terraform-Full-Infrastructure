03ea5adf89d181b15560c7f713d058c58a3dffca
Ameer ALi <114929844+Ameer-clk@users.noreply.github.com>
2022-11-05 20:00:41 +0530

    Update Infra terraform.tf

--- infrastracture/Infra terraform.tf
+++ infrastracture/Infra terraform.tf
@@ -1,7 +1,7 @@
   provider "aws" {
   region = "us-east-1"
-  access_key = "hshahhsask"
-  secret_key = "73733838388"
+  access_key = "sndsdwdhdhsdsdskdsd"
+  secret_key = "sdakdjsdsjdkasdskdj"
 }
   resource "aws_vpc" "main" {
   cidr_block = "10.1.0.0/16"
@@ -43,16 +43,16 @@ resource "aws_ami" "example" {
   imds_support        = "v2.0"
   ebs_block_device {
     device_name = "/dev/xvda"
-    snapshot_id = "you can add your own snapshot id"
+    snapshot_id = "snap-09c3bd9a539a0c1fb"
      volume_size = 100
   }
 }
 
 resource "aws_launch_template" "mytemplate" {
   name = "mytemplate"
-  image_id = "ami-08c40ec9ead489470"
+  image_id = "ami-09d3b3274b6c5d4aa"
    instance_type = "t2.micro"
-   key_name = "demo"
+   key_name = "kubernetes"
 }
 
 resource "aws_lb_target_group" "newtargetgp" {
@@ -61,31 +61,32 @@ resource "aws_lb_target_group" "newtargetgp" {
   protocol = "HTTP"
   vpc_id   = aws_vpc.main.id
 }
+
 resource "aws_lb" "test" {
-  name               = "test-lb-tf"
+  name               = "test"
   internal           = false
   load_balancer_type = "application"
   security_groups    = [aws_security_group.serversg.id]
   subnets            = [aws_subnet.mysubnet1.id,aws_subnet.mysubnet2.id]
 }
 
-resource "aws_autoscaling_group" "newASG" {
+resource "aws_autoscaling_group" "myautoscalinggp" {
   availability_zones = ["us-east-1a"]
   desired_capacity   = 1
   max_size           = 2
   min_size           = 1
 
-  launch_template {
+ launch_template {
     id      = aws_launch_template.mytemplate.id
     version = "$Latest"
   }
 }
-
 resource "aws_sns_topic" "newtopic" {
   name = "newtopic"
   kms_master_key_id = "alias/aws/sns"
 }
  
+
 
 
 
