407d52f785d87d3b25654932e450582e8a22bdea
Ameer ALi <114929844+Ameer-clk@users.noreply.github.com>
2023-07-16 17:00:22 +0000

    update

--- hosting-app/app.tf
+++ hosting-app/app.tf
@@ -99,6 +99,29 @@ resource "aws_s3_bucket" "mybucket96339" {
   bucket = "mybucket96339"
 }
 
+resource "aws_s3_bucket_policy" "bucket_policy" {
+  bucket = aws_s3_bucket.mybucket96339.id
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Sid": "AllowDelete",
+      "Effect": "Allow",
+      "Principal": "*",
+      "Action": [
+        "s3:DeleteObject",
+        "s3:DeleteObjectVersion"
+      ],
+      "Resource": [
+        "arn:aws:s3:::${aws_s3_bucket.mybucket96339.id}/*"
+      ]
+    }
+  ]
+}
+EOF
+}
 resource "aws_lb_target_group" "tg" {
   name     = "my-target-group"
   port     = 80
@@ -154,6 +177,7 @@ resource "aws_instance" "web" {
   subnet_id                   = aws_subnet.privatesubnet.id
   availability_zone           = "us-east-1a"
   private_ip                  = "192.168.20.10"
+  vpc_id                      = aws_vpc.myvpc.id
   vpc_security_group_ids      = [aws_security_group.mysg.id]
 
   # Add the RDS instance as a dependency
