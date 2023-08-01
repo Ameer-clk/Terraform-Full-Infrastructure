3a59ebe6e114f4cacc97fd36959a4681f80355b7
Ameer ALi <114929844+Ameer-clk@users.noreply.github.com>
2023-08-01 12:22:34 +0400

    Update main.tf

--- Ec2_Rds/main.tf
+++ Ec2_Rds/main.tf
@@ -63,14 +63,14 @@ resource "aws_security_group" "example_sg" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["10.1.0.0/0"]
   }
 
   ingress {
     from_port   = 3306 
     to_port     = 3306
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["10.1.0.0/0"]
   }
 
   egress {
