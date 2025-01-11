b6deb75800e8b5543650ece352851c8abffbe087
Ameer <114929844+Ameer-clk@users.noreply.github.com>
2025-01-11 18:33:34 +0400

    Update elasticache_cluster.tf

--- complex/elasticache/elasticache_cluster.tf
+++ complex/elasticache/elasticache_cluster.tf
@@ -1,24 +1,21 @@
-resource "aws_elasticache_subnet_group" "redis_subnet_group" {
-  name       = "redis-subnet-group"
-  subnet_ids = ["subnet-024bb03db545aaf28", "subnet-0d1fde223f647df84"] # Replace with your private subnet IDs
-}
+#!/bin/bash
 
-resource "aws_elasticache_cluster" "redis_cache_cluster" {
-  cluster_id                 = "redis-cache-cluster"
-  engine                     = "redis"
-  engine_version             = "7.1"
-  node_type                  = "cache.t3.medium"
-  num_cache_nodes            = 1
-  parameter_group_name       = "default.redis7"
-  port                       = 6379
-  subnet_group_name          = aws_elasticache_subnet_group.redis_subnet_group.name
-  security_group_ids         = ["sg-0197fdecc5f3c5fd8"] # Replace with the live SG Id
-  maintenance_window         = "wed:03:00-wed:04:00"
-  snapshot_retention_limit   = 1
-  snapshot_window            = "01:30-02:30"
-  auto_minor_version_upgrade = false
-  availability_zone          = "us-east-2b" # Replace with your desired availability zone
-  ip_discovery               = "ipv4"
-  network_type               = "ipv4"
-  transit_encryption_enabled = false
-}
+# Variables
+KARPENTER_VERSION="0.16.3"
+KARPENTER_IAM_ROLE_ARN="arn:aws:iam::891377279275:role/KarpenterController-20241122064215956900000001"
+CLUSTER_NAME=	"prod-project636-cluster"
+CLUSTER_ENDPOINT= "https://5F314C28F5D79FC81B8895A85A19CA92.gr7.eu-west-2.eks.amazonaws.com"
+
+# Add Karpenter Helm repository
+helm repo add karpenter https://charts.karpenter.sh/
+helm repo update
+
+# Install or upgrade Karpenter
+helm upgrade --install --namespace karpenter --create-namespace \
+  karpenter karpenter/karpenter \
+  --version ${KARPENTER_VERSION} \
+  --set serviceAccount.annotations."eks\.amazonaws\.com/role-arn"=${KARPENTER_IAM_ROLE_ARN} \
+  --set clusterName=${CLUSTER_NAME} \
+  --set clusterEndpoint=${CLUSTER_ENDPOINT} \
+  --set aws.defaultInstanceProfile=KarpenterNodeInstanceProfile-${CLUSTER_NAME} \
+  --wait
