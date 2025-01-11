dee7b69f8269350a6f6f0b36a6f42eb52ecca397
Ameer <114929844+Ameer-clk@users.noreply.github.com>
2025-01-11 18:33:03 +0400

    Update main.tf

--- complex/eks/main.tf
+++ complex/eks/main.tf
@@ -2,13 +2,13 @@
 data "aws_vpc" "existing_vpc" {
   filter {
     name   = "tag:Name"
-    values = ["project636beta-vpc"]  # Replace with your existing VPC name
+    values = ["prod-project636-vpc"]  # Replace with your existing VPC name
   }
 }
 
 # IAM Role for EKS
-resource "aws_iam_role" "eks_role" {
-  name = "eks-cluster-role"
+resource "aws_iam_role" "eks_role_prod" {
+  name = "eks-cluster-role-prod"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
@@ -27,13 +27,170 @@ resource "aws_iam_role" "eks_role" {
 
 # Attach necessary policies to the IAM role
 resource "aws_iam_role_policy_attachment" "eks_role_policy" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
-  role       = aws_iam_role.eks_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSServicePolicy"
+  role       = aws_iam_role.eks_role_prod.name
 }
 
 resource "aws_iam_role_policy_attachment" "eks_vpc_resource_attachment" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_VPC_Resource_Controller"
-  role       = aws_iam_role.eks_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"  # Corrected policy ARN
+  role       = aws_iam_role.eks_role_prod.name
+}
+
+# Define the correct IAM Policy for EBS CSI Driver
+resource "aws_iam_policy" "prod_ebs_csi_driver_policy" {
+  name        = "AmazonEBSCSIDriverPolicy"
+  description = "Policy for EBS CSI driver for EKS"
+  policy      = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+        {
+            "Effect": "Allow",
+            "Action": [
+                "ec2:CreateSnapshot",
+                "ec2:AttachVolume",
+                "ec2:DetachVolume",
+                "ec2:ModifyVolume",
+                "ec2:DescribeAvailabilityZones",
+                "ec2:DescribeInstances",
+                "ec2:DescribeSnapshots",
+                "ec2:DescribeTags",
+                "ec2:DescribeVolumes",
+                "ec2:DescribeVolumesModifications"
+            ],
+            "Resource": "*"
+        },
+        {
+            "Effect": "Allow",
+            "Action": [
+                "ec2:CreateTags"
+            ],
+            "Resource": [
+                "arn:aws:ec2:*:*:volume/*",
+                "arn:aws:ec2:*:*:snapshot/*"
+            ],
+            "Condition": {
+                "StringEquals": {
+                    "ec2:CreateAction": [
+                        "CreateVolume",
+                        "CreateSnapshot"
+                    ]
+                }
+            }
+        },
+        {
+            "Effect": "Allow",
+            "Action": [
+                "ec2:DeleteTags"
+            ],
+            "Resource": [
+                "arn:aws:ec2:*:*:volume/*",
+                "arn:aws:ec2:*:*:snapshot/*"
+            ]
+        },
+        {
+            "Effect": "Allow",
+            "Action": [
+                "ec2:CreateVolume"
+            ],
+            "Resource": "*",
+            "Condition": {
+                "StringLike": {
+                    "aws:RequestTag/ebs.csi.aws.com/cluster": "true"
+                }
+            }
+        },
+        {
+            "Effect": "Allow",
+            "Action": [
+                "ec2:CreateVolume"
+            ],
+            "Resource": "*",
+            "Condition": {
+                "StringLike": {
+                    "aws:RequestTag/CSIVolumeName": "*"
+                }
+            }
+        },
+        {
+            "Effect": "Allow",
+            "Action": [
+                "ec2:DeleteVolume"
+            ],
+            "Resource": "*",
+            "Condition": {
+                "StringLike": {
+                    "ec2:ResourceTag/ebs.csi.aws.com/cluster": "true"
+                }
+            }
+        },
+        {
+            "Effect": "Allow",
+            "Action": [
+                "ec2:DeleteVolume"
+            ],
+            "Resource": "*",
+            "Condition": {
+                "StringLike": {
+                    "ec2:ResourceTag/CSIVolumeName": "*"
+                }
+            }
+        },
+        {
+            "Effect": "Allow",
+            "Action": [
+                "ec2:DeleteVolume"
+            ],
+            "Resource": "*",
+            "Condition": {
+                "StringLike": {
+                    "ec2:ResourceTag/kubernetes.io/created-for/pvc/name": "*"
+                }
+            }
+        },
+        {
+            "Effect": "Allow",
+            "Action": [
+                "ec2:DeleteSnapshot"
+            ],
+            "Resource": "*",
+            "Condition": {
+                "StringLike": {
+                    "ec2:ResourceTag/CSIVolumeSnapshotName": "*"
+                }
+            }
+        },
+        {
+            "Effect": "Allow",
+            "Action": [
+                "ec2:DeleteSnapshot"
+            ],
+            "Resource": "*",
+            "Condition": {
+                "StringLike": {
+                    "ec2:ResourceTag/ebs.csi.aws.com/cluster": "true"
+                }
+            }
+        }
+    ]
+  })
+}
+
+# Create the IAM Role for EBS CSI Driver
+resource "aws_iam_role" "prod_ebs_csi_driver_role" {
+  name = "prod-eks-cluster-ebs-csi-driver-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Service = "eks.amazonaws.com"
+        }
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
 }
 
 # Create the EKS Cluster
@@ -41,29 +198,102 @@ module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "~> 20.0"
 
-  cluster_name    = "project636beta-cluster"
+  cluster_name    = "prod-project636-cluster"
   cluster_version = "1.31"
 
   cluster_endpoint_public_access  = true
 
   vpc_id                   = data.aws_vpc.existing_vpc.id
-  subnet_ids               = ["subnet-065d6543f0041a40d", "subnet-079231faa0933269d", "subnet-06d3415e0b6fef022"] #Replace with actuall subnet ids
-  cluster_security_group_id = "project636beta-sg"  #Replace with security group
+  subnet_ids               = ["subnet-03a36fdb089ab69cb", "subnet-00a6ef6bd8e6537b2", "subnet-05897fac09bd7e86d" , "subnet-0aed652824549f4b0" , "subnet-00a41e4b4b1c7390a" , "subnet-0eba50fb2b77914c4"]
 
 
   eks_managed_node_groups = {
-    project636beta = {
+    prod-project636 = {
       ami_type       = "AL2023_x86_64_STANDARD"
-      instance_types = ["t3.large", "t3a.large"] 
+      instance_types = ["t3.large", "t3.large"]
       min_size       = 1
       max_size       = 2
       desired_size   = 2
+
+      #Enable node auto-repair # ensures only one node is updated at a time, which is critical to maintaining cluster availability.
+      update_config = {
+        max_unavailable = 1
+      }
     }
   }
-
+  
   enable_cluster_creator_admin_permissions = true
 
   tags = {
-    Name = "project636beta-cluster"
+    Name = "prod-project636-cluster"
   }
+}
+
+
+# Install the VPC CNI add-on
+resource "aws_eks_addon" "eks_vpc_cni_addon" {
+  cluster_name                = "prod-project636-cluster"
+  addon_name                  = "vpc-cni"
+  addon_version               = "v1.19.2-eksbuild.1" # Replace with a valid version if needed
+  resolve_conflicts_on_create = "OVERWRITE"
+  resolve_conflicts_on_update = "OVERWRITE"
+
+  # Remove unsupported configuration values
+  configuration_values = jsonencode({
+    # Only include valid configuration options here
+  })
+}
+
+resource "aws_eks_addon" "eks_coredns_addon" {
+  cluster_name                = "prod-project636-cluster"
+  addon_name                  = "coredns"
+  addon_version               = "v1.11.4-eksbuild.2"  # Replace with the latest supported version
+  resolve_conflicts_on_create = "OVERWRITE"
+  resolve_conflicts_on_update = "OVERWRITE"
+
+  # Configuration values if any can be added here
+  configuration_values = jsonencode({
+    # Only include valid configuration options here if needed
+  })
+}
+
+resource "aws_eks_addon" "eks_kube_proxy_addon" {
+  cluster_name                = "prod-project636-cluster"
+  addon_name                  = "kube-proxy"
+  addon_version               = "v1.31.3-eksbuild.2"  # Replace with the latest supported version
+  resolve_conflicts_on_create = "OVERWRITE"
+  resolve_conflicts_on_update = "OVERWRITE"
+
+  # Configuration values if any can be added here
+  configuration_values = jsonencode({
+    # Only include valid configuration options here if needed
+  })
+}
+
+resource "aws_eks_addon" "eks_ebs_csi_driver_addon" {
+  cluster_name                = "prod-project636-cluster"
+  addon_name                  = "aws-ebs-csi-driver"
+  addon_version               = "v1.38.1-eksbuild.1"  # Replace with the latest supported version
+  resolve_conflicts_on_create = "OVERWRITE"
+  resolve_conflicts_on_update = "OVERWRITE"
+  service_account_role_arn    = aws_iam_role.prod_ebs_csi_driver_role.arn
+
+  # Configuration values if any can be added here
+  configuration_values = jsonencode({
+    # Only include valid configuration options here if needed
+  })
+}
+
+# Addon for Node Monitoring Agent
+resource "aws_eks_addon" "eks_Node_monitoring_agent" {
+  cluster_name                = "prod-project636-cluster"
+  addon_name                  = "node-monitoring-agent"  # Addon name for Node Monitoring Agent
+  addon_version               = "v1.0.1-eksbuild.2"       # Replace with the latest supported version
+  resolve_conflicts_on_create = "OVERWRITE"
+  resolve_conflicts_on_update = "OVERWRITE"
+
+  # Configuration values specific to Node Monitoring Agent can be added here
+  configuration_values = jsonencode({
+    # Example configuration options, if required
+  })
 }
