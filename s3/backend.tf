78304ed1d251f4c89c47b49c3d3eaed3268f78ec
Ameer <114929844+Ameer-clk@users.noreply.github.com>
2025-02-05 18:20:54 +0000

    updates

---
+++ s3/backend.tf
@@ -0,0 +1,40 @@
+terraform {
+  backend "s3" {
+    bucket         = "terraformtfstate13"
+    key            = "terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "terraform-lock"
+    encrypt        = true
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "terraformtfstate13"
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+resource "aws_s3_bucket_versioning" "versioning" {
+  bucket = aws_s3_bucket.terraform_state.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_dynamodb_table" "terraform_lock" {
+  name         = "terraform-lock"  # Updated name to match backend config
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
+
