9c5b8c4c5e6f187e0c4da89eec0efbc908063e60
techme <ameeraligithub@gmail.com>
2025-02-10 18:07:36 +0400

    updates

--- gaming-infra/alb/lb_listener_certificate.tf
+++
@@ -1,4 +0,0 @@
-resource "aws_lb_listener_certificate" "example" {
-  listener_arn    = aws_lb_listener.example_https.arn # Add the certificate ARN
-  certificate_arn = aws_acm_certificate.example.arn
-}
