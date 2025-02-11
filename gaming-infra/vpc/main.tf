4acfc73cc39a2a792440e0cd4f225cfbd884500e
techme <ameeraligithub@gmail.com>
2025-02-11 14:44:58 +0400

    changes

--- gaming-infra/vpc/main.tf
+++ gaming-infra/vpc/main.tf
@@ -45,13 +45,12 @@ resource "aws_security_group" "prod-project636-sg" {
   }
 
   egress {
-    description      = "Allow all outbound traffic"
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["10.0.0.0/16"]  # Restrict to known, trusted destination
   }
-
+  
   tags = {
     Name        = "prod-project636-sg"
     Environment = "dev"
